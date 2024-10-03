3be87a9e5b63f7fed9e1f627516f618b228854d9
Edirimanna <sajithedirimanna75@gmail.com>
2024-10-03 16:17:59 +0300

    Add the log line for the testing

--- src/main/java/com/cafe/CafeManagemenetSystemApplication.java
+++ src/main/java/com/cafe/CafeManagemenetSystemApplication.java
@@ -9,6 +9,7 @@ public class CafeManagemenetSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CafeManagemenetSystemApplication.class, args);
 		System.out.println("Hello");
+		System.out.println("Test My application");
 	}
 
 }
